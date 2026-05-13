8c0978b9361c3ab70206ca2eeb17c220062b5310
bhavanikankanala23 <bhavanikankanala23@gmail.com>
2026-05-12 21:43:14 -0500

    Remove duplicate migrations directory

--- lib/migrations/001_add_publish_at_and_public_id.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE documents ADD COLUMN publish_at TEXT;
-ALTER TABLE documents ADD COLUMN public_id TEXT;
-
-CREATE UNIQUE INDEX IF NOT EXISTS idx_documents_public_id
-ON documents(public_id);
-
-CREATE INDEX IF NOT EXISTS idx_documents_title
-ON documents(title);
